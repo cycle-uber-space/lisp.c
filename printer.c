9dcf69d7d7db6a94e9ababb535b029e8d33a24f3
Cycle <cycle@uber.space>
2021-04-26 21:14:15 +0200

    add cons to printer

--- printer.c
+++ printer.c
@@ -3,6 +3,34 @@
 
 void render_expr(Expr exp, Expr out);
 
+void render_cons(Expr exp, Expr out)
+{
+    stream_put_char(out, '(');
+    render_expr(car(exp), out);
+
+    for (Expr tmp = cdr(exp); tmp; tmp = cdr(tmp))
+    {
+        if (tmp == exp)
+        {
+            stream_put_string(out, " ...");
+            break;
+        }
+        else if (is_cons(tmp))
+        {
+            stream_put_char(out, ' ');
+            render_expr(car(tmp), out);
+        }
+        else
+        {
+            stream_put_string(out, " . ");
+            render_expr(tmp, out);
+            break;
+        }
+    }
+
+    stream_put_char(out, ')');
+}
+
 void render_expr(Expr exp, Expr out)
 {
     switch (expr_type(exp))
@@ -13,6 +41,9 @@ void render_expr(Expr exp, Expr out)
         break;
     case TYPE_SYMBOL:
         stream_put_string(out, symbol_name(exp));
+        break;
+    case TYPE_CONS:
+        render_cons(exp, out);
         break;
     default:
         LISP_FAIL("cannot print expression %016" PRIx64 "\n", exp);
