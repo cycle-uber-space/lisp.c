01c00a67d0b25bc8163cc72742dd1a81d036548c
Cycle <cycle@uber.space>
2021-04-26 17:34:26 +0200

    add unit testing framework

--- lisp.c
+++ lisp.c
@@ -1,11 +1,22 @@
 
+/* lisp.h */
+
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include <assert.h>
 #include <string.h>
+
+#define LISP_RED     "\x1b[31m"
+#define LISP_GREEN   "\x1b[32m"
+#define LISP_YELLOW  "\x1b[33m"
+#define LISP_BLUE    "\x1b[34m"
+#define LISP_MAGENTA "\x1b[35m"
+#define LISP_CYAN    "\x1b[36m"
+#define LISP_RESET   "\x1b[0m"
 
 typedef uint8_t U8;
 typedef uint16_t U16;
@@ -33,6 +44,78 @@ static_assert(sizeof(I64) == 8, "");
 static_assert(sizeof(F32) == 4, "");
 static_assert(sizeof(F64) == 8, "");
 
+/* test.h */
+
+typedef struct
+{
+    int num_tests;
+    int num_failed;
+    bool show_pass;
+    bool exit_on_fail;
+} TestState;
+
+#define LISP_TEST_GROUP(test, text) test_group(test, text)
+#define LISP_TEST_BEGIN(test)       test_begin(test)
+#define LISP_TEST_FINISH(test)      test_finish(test)
+#define LISP_TEST_ASSERT(test, exp) do { test_assert_try(test, exp, #exp); } while (0)
+
+void test_begin(TestState * test);
+void test_finish(TestState * test);
+void test_group(TestState * test, char const * text);
+void test_assert_try(TestState * test, bool exp, char const * msg);
+
+/* test.c */
+
+#define LISP_TEST_FILE stdout
+
+void test_begin(TestState * test)
+{
+    test->num_tests    = 0;
+    test->num_failed   = 0;
+    test->show_pass    = true;
+    test->exit_on_fail = false;
+}
+
+void test_finish(TestState * test)
+{
+    if (test->num_failed)
+    {
+        fprintf(LISP_TEST_FILE, LISP_RED "FAIL" LISP_RESET " " "%d/%d test(s)\n", test->num_failed, test->num_tests);
+    }
+    else
+    {
+        fprintf(LISP_TEST_FILE, LISP_GREEN "PASS" LISP_RESET " " "%d/%d test(s)\n", test->num_tests, test->num_tests);
+    }
+}
+
+void test_group(TestState * test, char const * text)
+{
+    fprintf(LISP_TEST_FILE, "==== %s ====\n", text);
+}
+
+void test_assert_try(TestState * test, bool exp, char const * msg)
+{
+    ++test->num_tests;
+    if (exp)
+    {
+        if (test->show_pass)
+        {
+            fprintf(LISP_TEST_FILE, LISP_GREEN "PASS" LISP_RESET " %s\n", msg);
+        }
+    }
+    else
+    {
+        ++test->num_failed;
+        fprintf(LISP_TEST_FILE, LISP_RED "FAIL" LISP_RESET " %s\n", msg);
+        if (test->exit_on_fail)
+        {
+            exit(1);
+        }
+    }
+}
+
+/* main.c */
+
 static void fail(char const * fmt, ...)
 {
     if (fmt)
@@ -50,8 +133,10 @@ static void fail(char const * fmt, ...)
     exit(1);
 }
 
-static void unit_test()
+static void unit_test(TestState * test)
 {
+    LISP_TEST_BEGIN(test);
+    LISP_TEST_FINISH(test);
 }
 
 int main(int argc, char ** argv)
@@ -63,7 +148,8 @@ int main(int argc, char ** argv)
     char const * cmd = argv[1];
     if (!strcmp("unit", cmd))
     {
-        unit_test();
+        TestState test;
+        unit_test(&test);
     }
     else
     {
