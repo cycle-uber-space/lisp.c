eb0c9f8a8d348c3d2dabb9c4d52707ff14c39782
Cycle <cycle@uber.space>
2021-04-27 22:49:31 +0200

    clean up

--- eval.c
+++ eval.c
@@ -133,10 +133,6 @@ Expr apply(Expr name, Expr args, Expr env)
 
 Expr eval(Expr exp, Expr env)
 {
-dispatch:
-    //fprintf(stderr, "EVAL %016" PRIx64 "\n", exp);
-    //fprintf(stderr, "EVAL %s\n", repr(exp));
-
     if (exp == nil)
     {
         return nil;
@@ -153,35 +149,7 @@ dispatch:
         }
         return env_get(env, exp);
     case TYPE_CONS:
-#if LISP_EVAL_QUOTE
-        if (car(exp) == intern("quote"))
-        {
-            return cadr(exp);
-        }
-        else
-#endif
-#if LISP_EVAL_IF
-        if (car(exp) == intern("if"))
-        {
-            Expr const test = cadr(exp);
-            Expr const then = caddr(exp);
-            Expr const rest = cdddr(exp) ? cadddr(exp) : nil;
-            if (eval(test, env) != nil)
-            {
-                exp = then;
-                goto dispatch;
-            }
-            else
-            {
-                exp = rest;
-                goto dispatch;
-            }
-        }
-        else
-#endif
-        {
-            return apply(car(exp), cdr(exp), env);
-        }
+        return apply(car(exp), cdr(exp), env);
     default:
         LISP_FAIL("cannot evaluate %s\n", repr(exp));
         return nil;
